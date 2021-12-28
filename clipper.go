34de48597690f2a1d6e5653b7cf82bfc6a7c03fb
Connor Davenport <cdavenport626@gmail.com>
2021-12-28 16:11:24 -0500

    add simple clipper for web articles and other misc to save for later

---
+++ clipper.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+)
+
+var url = flag.String("u", "", "url of thing to clip")
+var name = flag.String("n", "", "name of clipping (both file and field in file)")
+var author = flag.String("a", "", "author of thing (optional)")
+var category = flag.String("c", "Article", "category of thing (default is Article)")
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+// TODO: add error handling for missing name
+
+func main() {
+	flag.Parse()
+	fp := "/Users/connor.davenport/secondbrain/3 Resources/Unsorted"
+	f, err := os.Create(fp + "/" + *name + ".md")
+	check(err)
+
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	_, err = w.WriteString("## Name\n" + *name + "\n")
+	check(err)
+	_, err = w.WriteString("## Author\n" + *author + "\n")
+	check(err)
+	_, err = w.WriteString("## Category\n" + *category + "\n")
+	check(err)
+	_, err = w.WriteString("## URL\n" + *url + "\n")
+	check(err)
+	_, err = w.WriteString("## Tags\n#unread\n")
+	check(err)
+	_, err = w.WriteString("## Ratings/Comments\n\n")
+	check(err)
+	w.Flush()
+}
